6affd70310c5e638a1dc81bab10b65f1da055475
knightpy <knightPy@163.com>
2020-04-01 17:25:15 +0800

    first commit shadow

---
+++ PYBJShadowPath.framework/Headers/PYBJShadowPath.h
@@ -0,0 +1,19 @@
+//
+//  PYBJShadowPath.h
+//  PYBJShadowPath
+//
+//  Created by QJ on 2020/4/1.
+//  Copyright © 2020 QJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PYBJShadowPath.
+FOUNDATION_EXPORT double PYBJShadowPathVersionNumber;
+
+//! Project version string for PYBJShadowPath.
+FOUNDATION_EXPORT const unsigned char PYBJShadowPathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PYBJShadowPath/PublicHeader.h>
+
+#import <PYBJShadowPath/UIView+PYShadowPath.h>
